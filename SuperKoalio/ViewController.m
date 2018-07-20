e949af66036bf56e003fb9a9680ebfb4011a4150
Nick VanCise <nickvancise@nicks-MacBook-Pro.local>
2018-07-20 15:16:08 -0700

    to voew draw calls

--- SuperKoalio/ViewController.m
+++ SuperKoalio/ViewController.m
@@ -20,7 +20,7 @@
   SKView * skView = (SKView *)self.view;
   skView.showsFPS = YES;
   skView.showsNodeCount = YES;
-  
+  skView.showsDrawCount =YES;
   
   // Create and configure the scene.
   /*SKScene * scene = [GameLevelScene sceneWithSize:CGSizeMake((CGFloat)skView.bounds.size.width/1.2, ((CGFloat)skView.bounds.size.height/1.2))];  //was skView.bounds.size
